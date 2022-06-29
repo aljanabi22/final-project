bac60cf52ad080ffda2ea11036926b3ff845699a
aneltubek <tubeanel@gmail.com>
2022-06-29 11:15:07 +1000

    js file created , assigned variables

---
+++ main.js
@@ -0,0 +1,13 @@
+const firstName = document.getElementsByClassName('firstName');
+const lastName = document.getElementsByClassName('lastName');
+const description = document.getElementsByClassName('description');
+const assignedTo = document.getElementsByClassName('assignedTo');
+const dueDate = document.getElementsByClassName('dueDate');
+const submitButton = document.getElementsByClassName('submitButton');
+
+
+submitButton.addEventListener('click', function());
+
+function() {
+    if(this.firstName && this.lastName && this.description)
+}
